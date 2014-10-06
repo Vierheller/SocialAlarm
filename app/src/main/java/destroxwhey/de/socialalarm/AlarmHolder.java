5eab098b2fdc8911669baac2cca8f636f3230895
GTS2014Projekt <GTS.Projekt@outlook.de>
2014-10-06 20:23:43 +0200

    Added a AlarmHolder

---
+++ app/src/main/java/destroxwhey/de/socialalarm/AlarmHolder.java
@@ -0,0 +1,8 @@
+package destroxwhey.de.socialalarm;
+
+/**
+ * Created by Jan-Niklas on 06.10.2014.
+ */
+public class AlarmHolder {
+    
+}
--- app/src/main/java/destroxwhey/de/socialalarm/ContactHolder.java
+++ app/src/main/java/destroxwhey/de/socialalarm/ContactHolder.java
@@ -4,4 +4,11 @@ package destroxwhey.de.socialalarm;
  * Created by Jan-Niklas on 06.10.2014.
  */
 public class ContactHolder {
+    private String username;
+    private String email;
+
+    public ContactHolder(String username, String email){
+        this.username=username;
+        this.email=email;
+    }
 }
